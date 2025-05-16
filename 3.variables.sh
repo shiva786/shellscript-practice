7f0c31f36a986c746e4b7cdb601856e15f38a8b5
shivashankar <shiva@gmail.com>
2025-05-16 12:20:46 +0530

    new file added conversation

---
+++ 3.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=shiva
+PERSON2=somu
+
+echo "$PERSON1: hello $PERSON2 ,how are you"
+echo "$PERSON2: I'm fine how about you"
+echo "$PERSON1:I'm goog ra, will you come goa"
+echo "$PERSON2: No ra, i don't have money"
